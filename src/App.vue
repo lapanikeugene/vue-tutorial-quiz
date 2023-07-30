2630165770aa2b024f5bf072225bf7fd8c3d3140
lapanikeugene <eugeny.lp@gmail.com>
2023-07-30 10:26:23 +0200

    vue-tutorial-quiz

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import {ref, computed} from "vue";
+  const questions = ref([
+    {question:"What is Bahnhof?", 
+    answer:0, 
+    options:["Railway station","supermarket","pupil","school"],
+    selected:null}
+  ])
+</script>
+
+<template>
+  <h1>
+    Hello World!
+  </h1>
+</template>
+
+<style scoped>
+*{
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  background-color: #456b7c;
+  color: white;
+}
+</style>
